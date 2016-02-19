11612f871df1eb34c2e3781d9cc3205690e96c5f
Georg Kasper <georg.kasper08@gmail.com>
2016-02-19 09:28:56 -0800

    added FizzBuzz with UnitTest
    FizzBuzz is one popular Interview Question, hence I created a possible
    solution using modulo - The return is an Array with Numbers Fizz Buzz
    and FizzBuzz Strings.
    
    FizzBuzzTest is checking this array wether all Fizz Buzz and FizzBuzzes
    are at the right position. At the moment we don't check for numbers.

---
+++ src/ctci_tests/FizzBuzzTest.java
@@ -0,0 +1,39 @@
+package ctci_tests;
+
+import org.junit.*;
+import ctci_fizzbuzz.FizzBuzz;
+import static org.junit.Assert.*;
+
+/**
+ *  
+ * @author mdaaaa
+ *
+ */
+public class FizzBuzzTest {
+	
+	FizzBuzz fizz = new FizzBuzz(15);
+	String[] output = fizz.getFizzBuzzed();
+	
+	String fizzString = "Fizz";
+	String buzzString = "Buzz";
+	String fizzBuzzString = "FizzBuzz";
+
+	@Test
+	public void testFizzes() {
+		assertEquals(fizzString, output[3-1]);
+		assertEquals(fizzString, output[6-1]);
+		assertEquals(fizzString, output[9-1]);
+		assertEquals(fizzString, output[12-1]);
+	}
+	
+	@Test
+	public void testBuzzes() {
+		assertEquals(buzzString, output[5-1]);
+		assertEquals(buzzString, output[10-1]);
+	}
+	
+	@Test
+	public void testFizzBuzzes() {
+		assertEquals(fizzBuzzString, output[15-1]);
+	}
+}
