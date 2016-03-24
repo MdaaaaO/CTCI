2f5cd968c1df7acd84e43bfaaaaa35285fbc8d3f
Georg Kasper <georg.kasper08@gmail.com>
2016-03-24 16:50:07 +0100

    minor changes

--- tests/ctci_tests/FizzBuzzTest.java
+++ tests/ctci_tests/FizzBuzzTest.java
@@ -5,15 +5,15 @@ import ctci_fizzbuzz.FizzBuzz;
 import static org.junit.Assert.*;
 
 /**
- *  
+ *
  * @author mdaaaa
  *
  */
 public class FizzBuzzTest {
-	
+
 	FizzBuzz fizz = new FizzBuzz(15);
 	String[] output = fizz.getFizzBuzzed();
-	
+
 	String fizzString = "Fizz";
 	String buzzString = "Buzz";
 	String fizzBuzzString = "FizzBuzz";
@@ -25,13 +25,13 @@ public class FizzBuzzTest {
 		assertEquals(fizzString, output[9-1]);
 		assertEquals(fizzString, output[12-1]);
 	}
-	
+
 	@Test
 	public void testBuzzes() {
 		assertEquals(buzzString, output[5-1]);
 		assertEquals(buzzString, output[10-1]);
 	}
-	
+
 	@Test
 	public void testFizzBuzzes() {
 		assertEquals(fizzBuzzString, output[15-1]);
